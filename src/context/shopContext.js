0aedfed69d1e75e09b338aa57b67daccbd2cfd7e
Saeedeh Karimi <saeedehkarimi1994@gmail.com>
2025-11-24 22:44:48 +0100

    Updated cart.jsx

--- src/context/shopContext.js
+++ src/context/shopContext.js
@@ -24,7 +24,7 @@ export const ShopContextProvider = (props)=>{
     const removeFromCart = (itemId)=> {
         setCartItems(cartItems.map((item)=> {
             if(item.id === itemId)
-                return {...item, count: item.count -1};
+                return {...item, count: item.count===0?0 : item.count -1};
             return item;
         }));
 
--- src/pages/cart/cart.jsx
+++ src/pages/cart/cart.jsx
@@ -1,6 +1,24 @@
+import React from "react";
+import { Products } from "../../data/products";
+import { useContext } from "react";
+import { ShopContext } from "../../context/shopContext";
+import Product from "../shop/product";
 const Cart = ()=>{
+    const {cartItems} = useContext(ShopContext)
     return(
-        <h1>cart page</h1>
+        <React.Fragment>
+        <h1>Your Cart Items</h1>
+        <div className="row">
+          {
+            Products.map((product)=>{
+                if(cartItems.find(cartItem => cartItem.id === product.id && cartItem.count > 0))
+                    return <Product data={product}/>
+
+            })
+          }
+
+        </div>
+        </React.Fragment>
     )
 }
 export default Cart;
